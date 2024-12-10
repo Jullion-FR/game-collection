4016ded1862425f55c2b1d57516d707967a82f3c
Julien DAUVERGNE <judau27@gmail.com>
2024-12-10 16:25:33 +0100

    font import

--- index.php
+++ index.php
@@ -7,11 +7,5 @@ use Dotenv\Dotenv;
 $dotenv = Dotenv::createImmutable(__DIR__);
 $dotenv->load();
 
-// Accédez aux variables d'environnement
-$dbHost = $_ENV['DB_HOST'];
-$dbUser = $_ENV['DB_USER'];
-$dbPass = $_ENV['DB_PASS'];
-
-echo "Database Host: $dbHost";
-
+require 'pages/view/landing.php';
 ?>
---
+++ pages/view/header.php
@@ -0,0 +1,17 @@
+<header>
+    <img src="assets/images/logo.png" alt="LOGO">
+    <ul>
+        <li>
+            <a href="">MA BIBLIOTHEQUE</a>
+        </li>
+        <li>
+            <a href="">AJOUTER UN JEU</a>
+        </li>
+        <li>
+            <a href="">CLASSEMENT</a>
+        </li>
+        <li>
+            <a href="">PROFIL</a>
+        </li>
+    </ul>
+</header>
