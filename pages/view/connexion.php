341dc734dc765e2e2510e7e9e75edebc4487d023
Floriane <floriane.petibon@free.fr>
2024-12-10 15:17:14 +0100

    ajout de connexion.php

---
+++ pages/view/connexion.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Connexion</title>
+</head>
+<body>
+    <div class="container">
+        <h1>Se connecter a Game Collection</h1>
+        <label for="mail">Email :</label>
+        <input type="email" name="user_email" />
+        <label for="password">Mot de passe :</label>
+        <input type="password" name="user_password" />
+        <form action="#" method="post">
+            <div class="button">
+                <button type="submit">SE CONNECTER</button>
+            </div>
+        </form>
+    </div>
+    <a href="#">S'incrire</a>
+</body>
+</html>
